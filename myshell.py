aad64259da274b684731854c7d4ada19aa6fcb39
stasi <stasi009@163.com>
2024-04-25 11:35:02 +0800

    save

--- myshell.py
+++ myshell.py
@@ -32,17 +32,17 @@ class Render:
 @dataclass
 class Input:
     name: str
-    # 'text': user will be prompted to input data 
+    # 'text': user will be prompted to input data
     # 'IM' : user needs to type in the chat to provide an input.
     type: str
-    # If user_input is false, user will not be prompted to input via a form, 
+    # If user_input is false, user will not be prompted to input via a form,
     # a new variable with the value of default_value will be automatically generated
     user_input: bool
     default_value: str
-    
+
     def __post_init__(self):
-        if self.type not in ('text','IM'):
-            raise ValueError('input type can only be text or IM')
+        if self.type not in ("text", "IM"):
+            raise ValueError("input type can only be text or IM")
 
     def value_dict(self) -> dict[str, Any]:
         d = asdict(self)
@@ -62,23 +62,28 @@ class AtomicState:
     def render(self, render: Render) -> None:
         self.__render = render
 
+    @property
     def name(self) -> str:
         return self.__name
 
     def transit(self, action, new_state):
         self.__transitions[action] = new_state
-        
-    def add_input(self,input:Input)->None: 
+
+    def input(self, input: Input) -> None:
         self.__inputs[input.name] = input.value_dict()
-        
-    def add_output(self,name:str,value:str)->None: 
-        self.__outputs[name] = value
+
+    def output(self, name: str, value: str, store_context: bool) -> None:
+        if store_context:
+            name = "context." + name
+        self.__outputs[name] = "{{" + value + "}}"
 
     def to_dict(self) -> dict[str, Any]:
+        """ inputs -> tasks -> outputs -> render
+        """
         return {
             "inputs": self.__inputs,
-            "outputs": self.__outputs,
             "tasks": self.__tasks,
+            "outputs": self.__outputs,
             "render": self.__render.to_dict(),
             "transitions": self.__transitions,
         }
--- test_myshell.py
+++ test_myshell.py
@@ -1,4 +1,4 @@
-from myshell import Button, Render, AtomicState,Input
+from myshell import Button, Render, AtomicState, Input
 import json
 
 
@@ -10,12 +10,13 @@ def test_render():
     btn = Button(content="Start", description="Click to Start.", on_click="start_demo")
     render.add_button(btn)
 
-    print(json.dumps(render.to_dict(),indent=4))
-    
+    print(json.dumps(render.to_dict(), indent=4))
+
+
 def test_atomic_state():
-    state = AtomicState(name='home_page_state')
-    state.transit(action='start_demo',new_state='home_page_state')
-    
+    state = AtomicState(name="home_page_state")
+    state.transit(action="start_demo", new_state="home_page_state")
+
     # ------- create render
     render = Render()
     render.add_text("Hello World! Welcome to this demo. Click 'Start' to chat!")
@@ -23,22 +24,61 @@ def test_atomic_state():
     btn = Button(content="Start", description="Click to Start.", on_click="start_demo")
     render.add_button(btn)
     # ------- END create render
-    
-    state.render(render)
-    
-    print(json.dumps(state.to_dict(),indent=4))
 
-def test_input():
-    intro_message = Input(name='intro_message',
-                          type='text',
-                          user_input=True,
-                          default_value='Hi, this is your Pro Config Tutorial Bot')
-    print(intro_message)
-    
-    
+    state.render(render)
+
+    print(json.dumps(state.to_dict(), indent=4))
+
+
+class TestAtomicState1:
+    def __init__(self) -> None:
+        self._state = AtomicState(name="home_page_state")
+
+    def __add_inputs(self):
+        intro_message = Input(
+            name="intro_message",
+            type="text",
+            user_input=True,
+            default_value="Hi, this is your Pro Config Tutorial Bot",
+        )
+        self._state.input(intro_message)
+
+        tts_widget_url = Input(
+            name="tts_widget_url",
+            type="text",
+            user_input=True,
+            default_value="https://app.myshell.ai/widget/mEjUNr",
+        )
+        self._state.input(tts_widget_url)
+        
+    def __add_outputs(self):
+        self._state.output(name='intro_message',value='intro_message',store_context=False)
+        self._state.output(name='voice_id',value='tts_widget_url',store_context=False)
+        
+    def __render(self):
+        render = Render()
+        render.add_text("Hello World! Welcome to this demo. Click 'Start' to chat!")
+
+        btn = Button(content="Start", description="Click to Start.", on_click="start_demo")
+        render.add_button(btn)
+        
+        self._state.render(render)
+        
+    def __add_transitions(self):
+        self._state.transit(action='start_demo',new_state='home_page_state')
+        
+    def run(self):
+        """ inputs -> tasks -> outputs -> render
+        """
+        self.__add_inputs()
+        self.__add_outputs()
+        self.__render()
+        self.__add_transitions()
+        
+        print(json.dumps(self._state.to_dict(), indent=4))
 
 
 if __name__ == "__main__":
     # test_render()
     # test_atomic_state()
-    test_input()
+    TestAtomicState1().run()
