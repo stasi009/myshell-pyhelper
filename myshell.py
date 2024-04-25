94fdc5152dd1604448233b02bb2fd25e657e6346
stasi <stasi009@163.com>
2024-04-25 10:52:48 +0800

    save

--- myshell.py
+++ myshell.py
@@ -1,44 +1,57 @@
-
-from dataclasses import dataclass,asdict
+from dataclasses import dataclass, asdict
 from typing import Any
 
 
 @dataclass
 class Button:
-    content:str 
-    description:str 
-    on_click:str
-    
-    
+    content: str
+    description: str
+    on_click: str
+
+
 class Render:
     def __init__(self) -> None:
         self._renders = {}
-        
-    def add_text(self,text:str):
-        self._renders['text'] = text
-        
-    def add_button(self,btn: Button): 
+
+    def add_text(self, text: str):
+        self._renders["text"] = text
+
+    def add_button(self, btn: Button):
         btn_list = None
         if "buttons" not in self._renders:
             btn_list = []
-            self._renders['buttons'] = btn_list
+            self._renders["buttons"] = btn_list
         else:
-            btn_list = self._renders['buttons']
+            btn_list = self._renders["buttons"]
         btn_list.append(asdict(btn))
-        
-    def to_dict(self):
+
+    def to_dict(self) -> dict[str, Any]:
         return self._renders
 
-@dataclass
-class State:
-    name:str 
-    inputs: dict[str,Any]
-    outputs: dict[str,Any]
-    tasks: list[Any]
-    render: dict[str,Any]
-    transitions:dict[str,str]
-    
 
-    
-    
+class AtomicState:
+    def __init__(self, name: str) -> None:
+        self.__name = name
+        self.__inputs = {}
+        self.__outputs = {}
+        self.__tasks = []
+        self.__render: Render = None
+        self.__transitions = {}
 
+    def render(self, render: Render) -> None:
+        self.__render = render
+        
+    def name(self)->str: 
+        return self.__name
+    
+    def transit(self,action, new_state):
+        self.__transitions[action] = new_state
+
+    def to_dict(self) -> dict[str, Any]:
+        return {
+            "inputs": self.__inputs,
+            "outputs": self.__outputs,
+            "tasks": self.__tasks,
+            "render": self.__render.to_dict(),
+            "transitions": self.__transitions,
+        }
--- test_myshell.py
+++ test_myshell.py
@@ -1,4 +1,4 @@
-from myshell import Button, Render
+from myshell import Button, Render, AtomicState
 import json
 
 
@@ -11,7 +11,27 @@ def test_render():
     render.add_button(btn)
 
     print(json.dumps(render.to_dict(),indent=4))
+    
+def test_atomic_state():
+    state = AtomicState(name='home_page_state')
+    state.transit(action='start_demo',new_state='home_page_state')
+    
+    # ------- create render
+    render = Render()
+    render.add_text("Hello World! Welcome to this demo. Click 'Start' to chat!")
+
+    btn = Button(content="Start", description="Click to Start.", on_click="start_demo")
+    render.add_button(btn)
+    # ------- END create render
+    
+    state.render(render)
+    
+    print(json.dumps(state.to_dict(),indent=4))
+
+    
+    
 
 
 if __name__ == "__main__":
-    test_render()
+    # test_render()
+    test_atomic_state()
