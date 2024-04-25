518c4bdb3fa6f6aea47310dfd3b49aaa409143ff
stasi <stasi009@163.com>
2024-04-25 10:35:15 +0800

    save

--- test_myshell.py
+++ test_myshell.py
@@ -1,2 +1,17 @@
-from myshell import Render
+from myshell import Button, Render
+import json
 
+
+def test_render():
+
+    render = Render()
+    render.add_text("Hello World! Welcome to this demo. Click 'Start' to chat!")
+
+    btn = Button(content="Start", description="Click to Start.", on_click="start_demo")
+    render.add_button(btn)
+
+    print(json.dumps(render.to_dict(),indent=4))
+
+
+if __name__ == "__main__":
+    test_render()
