c06454ae77e577f8af6d171d30c286d30afbba61
stasi <stasi009@163.com>
2024-04-26 10:28:34 +0800

    save

---
+++ proconfig_quiz/chat_state.py
@@ -0,0 +1,60 @@
+
+from myshell import *  
+from proconfig_quiz.constants import States
+
+class ChatPageState:
+    def __init__(self) -> None:
+        self._state = AtomicState(States.chat_page_state)
+
+    def __add_tasks(self):
+        self._state.add_task(
+            Module(
+                name="generate_reply",
+                module_type=ModuleType.AnyWidgetModule,
+                module_config={
+                    "widget_id": "1744214024104448000",
+                    "system_prompt": "You are a teacher teaching Pro Config.",
+                    "user_prompt": "{{user_message}}",
+                    "memory": "{{context.memory}}",
+                    "output_name": "reply",
+                },
+            )
+        )
+
+        self._state.add_task(
+            Module(
+                name="generate_voice",
+                module_type=ModuleType.AnyWidgetModule,
+                module_config={
+                    "content": "{{reply}}",
+                    "widget_id": "{{context.tts_widget_id}}",
+                    "output_name": "reply_voice",
+                },
+            )
+        )
+
+    def __render(self):
+        render = Render()
+        render.add_text("{{reply}}")
+        render.add_audio("{{reply_voice}}")
+        render.add_button(
+            # transition for 'go_home' is defined in automata
+            Button(content="Home", description="Click to Go Back to Home.", on_click="go_home")
+        )
+        self._state.render(render)
+
+    def build(self):
+        self._state.add_input(Input(name="user_message", type=InputType.IM, user_input=True))
+
+        self.__add_tasks()
+
+        self.__render()
+
+        self._state.add_output(
+            name="memory",
+            value="{{[...context.memory, {'user': user_message}, {'assistant': reply}]}}",
+            store_context=True,
+        )
+
+        self._state.transit(trigger=Trigger.CHAT, new_state=States.chat_page_state)
+        return self._state
